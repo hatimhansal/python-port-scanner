27b6e7a6d83be94076333aed003b247fa7d4ca49
Hatimhansal <hatimhatnsal50@gmail.com>
2026-07-25 17:37:27 +0100

    upgrade V2

--- Scan-Port/Sc_port.py
+++ Scan-Port/Sc_port.py
@@ -17,23 +17,24 @@ Python Port Scanner\n
     
 print(f" Host :{host}")
 host_ip_Address =socket.gethostbyname(host)
-print(f" Ip Address {host_ipAddress}")
+print(f" Ip Address {host_ip_Address}")
 print(f" Scanning ports {start_port} - {end_port} .....")
-for port in range (start_port,end_port+1):
-    s =socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-    s.settimeout(1)
-    resultat = s.connect_ex((host,port))
-        
-    if resultat == 0 :
-        try :
-            service_name=socket.getservbyport(port)
-            print(f"connect in port {port} Open  {service_name}")
-        except Exception as e :
-            service_name ='Unknown'
-            print(f"connect In port {port} Open  {service_name} ,{e}")
-    else :
-        print(f"  Port {port} CLOSED")
-    s.close()
+def scan_port(port):
+    for port in range (start_port,end_port+1):
+        s =socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        s.settimeout(1)
+        resultat = s.connect_ex((host,port))
+            
+        if resultat == 0 :
+            try :
+                service_name=socket.getservbyport(port)
+                print(f"connect in port {port} Open  {service_name}")
+            except Exception as e :
+                service_name ='Unknown'
+                print(f"connect In port {port} Open  {service_name} ,{e}")
+        else :
+            print(f"  Port {port} CLOSED")
+        s.close()
         
         
 
